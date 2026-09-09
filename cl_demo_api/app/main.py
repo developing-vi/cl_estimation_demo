f587356298850d2a72bd259343ca53682c97ff1e
developing-vi <143858600+developing-vi@users.noreply.github.com>
2026-09-09 11:26:20 +0800

    fix cors

--- cl_demo_api/app/main.py
+++ cl_demo_api/app/main.py
@@ -35,7 +35,7 @@ app = FastAPI(title="Cervical Length Estimation API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cl-estimation-demo.vercel.app/", "http://localhost:3000"],
+    allow_origins=["https://cl-estimation-demo.vercel.app", "http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
